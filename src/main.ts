d035708bdbaf5bd7ea1e51f94fe2b5af1568d555
SZanko <szanko_dev@proton.me>
2026-06-09 20:45:54 +0100

    Add triggers for PostCreate, CommentCreate, CommentUpdate
    Signed-off-by: SZanko <szanko_dev@proton.me>

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,85 @@ Devvit.configure({
 
 export default Devvit;
 
+Devvit.addTrigger({
+  event: 'PostCreate',
+  onEvent: async (event, context) => {
+    if(!event.post?.id) return;
+
+    const post = await context.reddit.getPostById(event.post.id);
+
+    const matches = searchForLink(
+      post.body ?? "",
+      event.post.id,
+      post.authorName ?? "",
+      context,
+    );
+
+    if (matches.length === 0) {
+      return;
+    }
+
+    const text = formatComments(matches);
+
+    await context.reddit.submitComment({ id: event.post.id, text });
+
+  }
+})
+
+Devvit.addTrigger({
+  event: "CommentCreate",
+  onEvent: async (event, context) => {
+    if (!event.post?.id) return;
+    if (!event.comment?.id) return;
+
+    const comment = await context.reddit.getCommentById(event.comment.id);
+
+    const matches = searchForLink(
+      comment.body ?? "",
+      event.comment.id,
+      comment.authorName ?? "",
+      context,
+    );
+
+    if (matches.length === 0) {
+      return;
+    }
+
+    const text = formatComments(matches);
+
+    await context.reddit.submitComment({ id: event.comment.id, text });
+  }
+})
+
+
+
+Devvit.addTrigger({
+  event: "CommentUpdate",
+  onEvent: async (event, context) => {
+    if (!event.post?.id) return;
+    if (!event.comment?.id) return;
+
+    const comment= await context.reddit.getCommentById(event.comment.id);
+
+    const matches = searchForLink(
+      comment.body ?? "",
+      event.comment.id,
+      comment.authorName ?? "",
+      context,
+    );
+
+    if (matches.length === 0) {
+      return;
+    }
+
+    const text = formatComments(matches);
+
+    await context.reddit.submitComment({ id: event.comment.id, text });
+  },
+});
+
+
+
 Devvit.addMenuItem({
   label: "Indie Wiki Buddy: Scan Post",
   location: "post",
